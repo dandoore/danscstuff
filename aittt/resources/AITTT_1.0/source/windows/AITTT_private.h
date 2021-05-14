f77df572fbf40866423abc8e01b6774a8d6615a8
dandoore <43847005+dandoore@users.noreply.github.com>
2021-05-14 13:31:34 +0100

    Add files via upload

---
+++ aittt/resources/AITTT_1.0/source/windows/AITTT_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef AITTT_PRIVATE_H
+#define AITTT_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"1.0.0.1"
+#define VER_MAJOR	1
+#define VER_MINOR	0
+#define VER_RELEASE	0
+#define VER_BUILD	1
+#define COMPANY_NAME	"Marcello Zaniboni"
+#define FILE_VERSION	""
+#define FILE_DESCRIPTION	"A.I. Tic Tac Toe"
+#define INTERNAL_NAME	""
+#define LEGAL_COPYRIGHT	"This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License; either version 2 of the License, or (at your option) any later version."
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	""
+#define PRODUCT_NAME	"A.I. Tic Tac Toe"
+#define PRODUCT_VERSION	""
+
+#endif /*AITTT_PRIVATE_H*/
